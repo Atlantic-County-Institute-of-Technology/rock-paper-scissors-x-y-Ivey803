b33c1797ec0b08a54a287c896b892b7a38a8dce5
ivey803 <spel0426@student.acitech.org>
2025-03-21 09:13:45 -0400

    added a lot of stuff

---
+++ script.js
@@ -0,0 +1,7 @@
+const text = document.getElementById("text");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const fridge = document.getElementById("fridge");
+const button1 = document.getElementById("computer");
+const container = document.getElementById("container")
